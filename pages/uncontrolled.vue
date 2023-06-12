ea2e115b4970a8234208f782c3ade3e7787cf4a5
Irsyaad Budi <irsyaad@privyid.tech>
2023-06-12 20:22:31 +0700

    feat: add uncontrolled form

---
+++ pages/uncontrolled.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="handleSubmit">
+      <div class="form-group">
+        <input name="firstName" />
+      </div>
+      <div class="form-group">
+        <input name="lastName" />
+      </div>
+      <button>Save</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+function handleSubmit(event) {
+  console.log(event.target);
+  const formData = new FormData(event.target);
+}
+</script>
+
+<style scoped></style>
